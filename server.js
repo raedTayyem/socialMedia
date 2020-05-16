03e4e76a3d5a500415197efe595d0e75e3ea2777
raedTayyem <62396352+raedTayyem@users.noreply.github.com>
2020-05-16 13:15:49 +0300

    update1

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
 const express = require('express');
+const connectDb = require('./config/db');
 
 const app = express()
+
+//connect Database
+connectDb();
+
+// Init Middleware
+app.use(express.json({
+    extend: false
+}));
+
+app.get('/', (req, res) => res.send('API Running'));
+
+// Define Routes 
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/users', require('./routes/api/users'));
 
 const PORT = process.env.PORT || 3040;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
-app.get('/',(req, res) => res.end('API Running'));
